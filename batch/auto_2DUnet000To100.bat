077c5ff6d1bac581ae2d14496a6de29bc0e9e38c
t-rryyyoooo <rryyyoooo@i.softbank.jp>
2019-11-05 21:12:57 +0900

    From windos

--- batch/auto_2DUnet000To100.bat
+++ batch/auto_2DUnet000To100.bat
@@ -12,19 +12,20 @@ set model=C:\Users\VMLAB\Desktop\KIDNEY\modelFolder\2DUnetModel_re
 set weight=C:\Users\VMLAB\Desktop\KIDNEY\weightFolder\best_re
 set history=C:\Users\VMLAB\Desktop\KIDNEY\history\history_re
 
-set rate[0]=0.0
-set rate[1]=0.05
-set rate[2]=0.10
-set rate[3]=0.15
-set rate[4]=0.20
-set rate[5]=0.25
-set rate[6]=0.30
-set rate[7]=0.35
-set rate[8]=0.40
-set rate[9]=0.45
+set rate[0]=0.50
+set rate[1]=0.55
+set rate[2]=0.60
+set rate[3]=0.65
+set rate[4]=0.70
+set rate[5]=0.80
+set rate[6]=0.85
+set rate[7]=0.90
+set rate[8]=0.95
+set rate[9]=1.0
+set rate[10]=0.75
 
 
-for %%i in (0,1,2,3,4,5,6,7,8,9) do (
+for %%i in (0,1,2,3,4,5,6,7,8,9,10) do (
     
     set trainingList=%training%!rate[%%i]!.txt
     set validationList=%validation%!rate[%%i]!.txt
@@ -41,6 +42,6 @@ for %%i in (0,1,2,3,4,5,6,7,8,9) do (
    
 
     python ../buildUnet3chAugmentation.py !trainingList! !models! -t !validationList!  -b 15  -e 40 --bestfile !weightfiles! --history !histories! 
-    python ../mail.py %history%
+    python ../mail.py %histories%
 )
 
