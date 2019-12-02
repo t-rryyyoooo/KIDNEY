486a648f226dd162915cd41201a5789b1b992867
t-rryyyoooo <rryyyoooo@i.softbank.jp>
2019-12-02 15:56:18 +0900

    Integrate Lab with AIC

--- auto2DUnetPerAlpha.sh
+++ auto2DUnetPerAlpha.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
 #Input
-readonly TRAINING="$HOME/Desktop/tanimoto/data/textList/training_"
-readonly VALIDATION="$HOME/Desktop/tanimoto/data/textList/validation_"
-readonly MODEL="$HOME/Desktop/tanimoto/data/model/model_"
-readonly WEIGHT="$HOME/Desktop/tanimoto/data/weight/best_"
-readonly HISTORY="$HOME/Desktop/tanimoto/data/history/history_"
+readonly TRAINING="$HOME/Desktop/data/textList/training_sum_float_"
+readonly VALIDATION="$HOME/Desktop/data/textList/validation_sum_float_"
+readonly WEIGHT="$HOME/Desktop/data/modelweight/best_sum_float_"
+readonly HISTORY="$HOME/Desktop/data/history/history_sum_float_"
 
-#readonly ALPHA=(0.55 0.60 0.65 0.70 0.80 0.85 0.90 0.95 1.0)
-readonly ALPHA=(1.0 0.95 0.90 0.85 0.80 0.75 0.70 0.65 0.60 0.55)
+echo -n GPU_ID:
+read id
+echo -n ALPHA=
+read ALPHA
 
-i=0
+
 for alpha in ${ALPHA[@]}
 do
+    for t in $(seq 0 4)
+    do
 
-    training=${TRAINING}${alpha}.txt
-    validation=${VALIDATION}${alpha}.txt
-    weight=${WEIGHT}${alpha}.hdf5
-    model=${MODEL}${alpha}.yml
-    histories=${HISTORY}${alpha}.txt
+        training=${TRAINING}${alpha}.txt
+        validation=${VALIDATION}${alpha}.txt
+        weight="${WEIGHT}${alpha}_${t}.hdf5"
+        #model='${MODEL}${alpha}_${t}.yml'
+        histories="${HISTORY}${alpha}_${t}.txt"
 
-    
-    
-    if [ $1 -eq $((i%2)) ]; then
-    echo $training
-    echo $validation
-    echo $weight
-    echo $model 
-    echo $histories
-    echo $alpha
-    python3 buildUnet3chAugmentation.py ${training} ${model} -t ${validation} --bestfile ${weight} --history ${histories} -b 15  -e 40 -g $1
-    python3 mail.py $histories
-    
-    else
-    echo $alpha skipped
+        
+        echo $training
+        echo $validation
+        echo $weight
+        #echo $model 
+        echo $histories
+        echo $alpha
 
-    fi
-    let i++
+        python3 buildUnet3chAugmentation.py ${training} ${weight} -t ${validation} --history ${histories} -b 15  -e 40 -g $id
+        #python3 mail.py $histories
+        
+
+    done
 
 done
--- autoTrainingAndSegmentationPerAlpha.sh
+++ autoTrainingAndSegmentationPerAlpha.sh
@@ -6,11 +6,17 @@ readonly VALIDATION="$HOME/Desktop/data/textList/validation_sum_float_"
 readonly WEIGHT="$HOME/Desktop/data/modelweight/best_sum_float_"
 readonly HISTORY="$HOME/Desktop/data/history/history_sum_float_"
 
+readonly DATA="$HOME/Desktop/data/kits19/data"
+readonly CT="/imaging.nii.gz"
+readonly LABEL="/segmentation.nii.gz"
+readonly SAVE="$HOME/Desktop/data/slice/summed_hist_float_"
+
+readonly NUMBERS=(173 002 068 133 155 114 090 105 112 175 183 208 029 065 157 162 141 062 031 156 189 135 020 077 000 009 198 036)
+
 echo -n GPU_ID:
 read id
 echo -n ALPHA=
 read ALPHA
-
 
 for alpha in ${ALPHA[@]}
 do
@@ -25,16 +31,28 @@ do
 
         
         echo $training
-        echo $validation
         echo $weight
-        #echo $model 
+        echo $validation
         echo $histories
         echo $alpha
 
         python3 buildUnet3chAugmentation.py ${training} ${weight} -t ${validation} --history ${histories} -b 15  -e 40 -g $id
-        #python3 mail.py $histories
-        
 
+        for number in ${NUMBERS[@]}
+        do 
+            save="${SAVE}${alpha}/segmentation/${t}/case_00${number}/label.mha"
+            ct="${DATA}/case_00${number}${CT}"
+            label="${DATA}/case_00${number}${LABEL}"
+
+            echo $label
+            echo $ct
+            echo $weight
+            echo $save
+            echo $alpha
+
+            python3 segmentationUnet3chAtOnce.py $label $ct $weight $save $alpha -g $id
+
+        done
     done
 
 done
