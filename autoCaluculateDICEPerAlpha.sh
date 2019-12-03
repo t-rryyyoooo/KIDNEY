5b817ccfcd0edf182d8f676e62846bf6aac08c45
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2019-12-03 13:59:06 +0900

    NJJ

--- autoCaluculateDICEPerAlpha.sh
+++ autoCaluculateDICEPerAlpha.sh
@@ -5,7 +5,7 @@ readonly TRUE="$HOME/Desktop/data/kits19"
 readonly RESULT="$HOME/Desktop/data/slice/hist_"
 #readonly ALPHA=(0.0 0.20 0.40 0.60 0.80 1.0)
 readonly TEXT="$HOME/Desktop/KIDNEY/result/"
-readonly PREFIX="summed_float_"
+readonly PREFIX="hist_"
 
 echo -n ALPHA=
 read ALPHA
