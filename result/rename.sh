e1ed37ad24df51dfc6d3a5b0218dea207f82d50c
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2019-12-09 18:29:03 +0900

    #

---
+++ result/rename.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+readonly OLD="summed_hist_float_"
+readonly NEW="summed_float_"
+
+alpha=(0.0 0.20 0.40 0.60 0.80 1.0)
+
+for a in ${alpha[@]}
+do 
+    for x in $(seq 5 9)
+    do
+	old="${OLD}${a}_${x}.txt"
+	new="${NEW}${a}_${x}.txt"
+
+	echo $old
+	echo $new
+	mv $old $new
+
+    done
+done
