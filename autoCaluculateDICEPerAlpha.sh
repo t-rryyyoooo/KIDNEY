b6a427807c33895abc1a3ac41536ef1fc7ae129f
t-rryyyoooo <rryyyoooo@i.softbank.jp>
2019-12-09 15:26:22 +0900

    DD

--- autoCaluculateDICEPerAlpha.sh
+++ autoCaluculateDICEPerAlpha.sh
@@ -2,10 +2,10 @@
 
 #Input
 readonly TRUE="$HOME/Desktop/data/kits19"
-readonly RESULT="$HOME/Desktop/data/slice/hist_"
+readonly RESULT="$HOME/Desktop/data/slice/summed_hist_float_"
 #readonly ALPHA=(0.0 0.20 0.40 0.60 0.80 1.0)
 readonly TEXT="$HOME/Desktop/KIDNEY/result/"
-readonly PREFIX="hist_"
+readonly PREFIX="summed_hist_float_"
 
 echo -n ALPHA=
 read ALPHA
